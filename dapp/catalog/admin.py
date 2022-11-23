52f6c0470d0767c2dd7380806c9892e1fcec1f32
anon <efimovsergeiv@gmail.com>
2022-11-23 11:15:39 +0300

    add:

--- dapp/catalog/admin.py
+++ dapp/catalog/admin.py
@@ -1,10 +1,24 @@
-from django import forms
 from django.contrib import admin
 from ckeditor.widgets import CKEditorWidget
-from parler.admin import TranslatableAdmin, TranslatableModelForm
+from mptt.admin import DraggableMPTTAdmin
+from mptt.forms import MPTTAdminForm
 from parler.forms import TranslatedField
+from parler.admin import TranslatableAdmin, TranslatableModelForm
 
-from catalog.models import ProductModel
+from catalog.models import ProductModel, ServiceModel, CategoryModel
+
+
+
+class CategoryAdminForm(MPTTAdminForm, TranslatableModelForm):
+    pass
+
+
+class CategoryAdmin(TranslatableAdmin, DraggableMPTTAdmin):
+    form = CategoryAdminForm
+
+    def get_prepopulated_fields(self, request, obj=None):
+        return {'slug': ('name',)}
+
 
 
 class ProductForm(TranslatableModelForm):
@@ -14,11 +28,32 @@ class ProductForm(TranslatableModelForm):
 class ProductAdmin(TranslatableAdmin):
     form = ProductForm
 
-    list_display = ('id', 'name',)
+    list_display = ('id', 'name', 'activated')
+    list_editable = ('activated',)
     fieldsets = (
-        (None, {'fields': ('name',)}),
+        (None, {'fields': (('name', 'priority', 'activated'),)}),
         (None, {'fields': ('description',)}),
     )
 
 
+
+class ServiceForm(TranslatableModelForm):
+    description = TranslatedField(label='Описание', widget=CKEditorWidget())
+
+
+class ServiceAdmin(TranslatableAdmin):
+    form = ServiceForm
+
+    list_display = ('id', 'name', 'activated',)
+    list_editable = ('activated',)
+    fieldsets = (
+        (None, {'fields': (('name', 'priority', 'activated'))}),
+        (None, {'fields': ('description',)}),
+    )
+
+admin.site.register(
+    CategoryModel, 
+    CategoryAdmin
+)
 admin.site.register(ProductModel, ProductAdmin)
+admin.site.register(ServiceModel, ServiceAdmin)
--- dapp/catalog/models.py
+++ dapp/catalog/models.py
@@ -1,10 +1,44 @@
 from django.db import models
-from ckeditor.fields import RichTextField
+from .managers import CategoryManager
+from mptt.models import MPTTModel, TreeForeignKey
 from parler.models import TranslatableModel, TranslatedFields
+
+
+
+class CategoryModel(MPTTModel, TranslatableModel):
+    """ 
+    Модель категорий 
+    Docs: https://django-parler.readthedocs.io/en/stable/advanced/mptt.html
+    """
+
+    activated = models.BooleanField(verbose_name="Активирован", default=False)
+    priority = models.IntegerField(verbose_name="Приоритет выдачи", default=50)
+    parent = TreeForeignKey('self', verbose_name="Вложенность", on_delete=models.CASCADE, null=True, blank=True, related_name="children")
+
+    translations = TranslatedFields(
+        name = models.CharField(verbose_name="Наименование", max_length=300),
+        slug = models.SlugField(blank=False, default='', max_length=300)
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    objects = CategoryManager()
+
+    # class MPTTMeta:
+    #     order_insertion_by = ('activated',)
+
+    def __str__(self):
+        return self.safe_translation_getter('name', any_language=True)
+
 
 
 class ProductModel(TranslatableModel):
     """ Модель товара каталога """
+
+    activated = models.BooleanField(verbose_name="Активирован", default=False)
+    priority = models.IntegerField(verbose_name="Приоритет выдачи", default=50)
 
     translations = TranslatedFields(
         name = models.CharField(verbose_name="Наименование", max_length=300),
@@ -14,6 +48,26 @@ class ProductModel(TranslatableModel):
     class Meta:
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
+
+    def __str__(self):
+        return self.name
+
+
+
+class ServiceModel(TranslatableModel):
+    """ Модель списка услуг """
+
+    activated = models.BooleanField(verbose_name="Активирован", default=False)
+    priority = models.IntegerField(verbose_name="Приоритет выдачи", default=50)
+
+    translations = TranslatedFields(
+        name = models.CharField(verbose_name="Наименование", max_length=300),
+        description = models.TextField(verbose_name="Описание", max_length=5000)
+    )
+
+    class Meta:
+        verbose_name = "Сервис"
+        verbose_name_plural = "Сервисы"
 
     def __str__(self):
         return self.name
