b0b2d7014c965bbc4c1fe19030a94b57be32f809
anon <efimovsergeiv@gmail.com>
2022-11-25 14:31:59 +0300

    add:

--- napp/pages/index.vue
+++ napp/pages/index.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="">
-    <nuxt-link :to="switchLocalePath('en')">English</nuxt-link>
-    <nuxt-link :to="switchLocalePath('de')">Deutsch</nuxt-link>
-    <nuxt-link :to="switchLocalePath('ru')">Русский</nuxt-link>
     <HeaderSection />
     <MainSliderSection />
     <FirstSection />
