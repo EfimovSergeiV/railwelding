55910bda9b0905b2bb7678520c014c6de3a51c3c
Sergei Efimov <efimovsergeiv@gmail.com>
2023-03-23 10:34:42 +0300

    add:

--- napp/components/sections/AboutSection.vue
+++ napp/components/sections/AboutSection.vue
@@ -2,7 +2,7 @@
   <footer class="mx-auto lg:max-w-7xl ">
     <div class="py-8 px-8  bg-white">
 
-      <div class="">
+      <!-- <div class="">
 
 
         <div class="flex justify-center py-4">
@@ -13,7 +13,7 @@
           <img src="maps.png" />
         </div>
         
-      </div>
+      </div> -->
       
       <div class="flex items-center justify-center">
         <p class="text-slate-900 uppercase text-xl font-bold">Компания Railwelding</p>
